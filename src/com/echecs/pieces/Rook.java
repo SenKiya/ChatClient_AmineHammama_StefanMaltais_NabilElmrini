f235364a1a15fe2651d1d0c92aeb2fa6169e127a
Nabil <nabilelmrini45@gmail.com>
2023-12-14 23:18:15 -0500

    Test Fix du repository

--- src/com/echecs/pieces/Rook.java
+++ src/com/echecs/pieces/Rook.java
@@ -19,47 +19,44 @@ public class Rook extends Piece {
         PartieEchecs partieEchecs = new PartieEchecs();
 
 
+        if (pos1.estSurLaMemeColonneQue(pos2) == (false) && pos1.estSurLaMemeLigneQue(pos2) == (false)) {
+            return false;
+        }
 
-            if (pos1.estSurLaMemeColonneQue(pos2) == (false) && pos1.estSurLaMemeLigneQue(pos2) == (false)) {
-                return false;
+
+        if (pos1.estSurLaMemeColonneQue(pos2)) {
+            if (ligne1 > ligne2) {
+                for (int i = ligne1; i > ligne2; i--) {
+                    if (echiquier[i][colonne1] != null) {
+                        return false;
+                    }
+
+                }
+            } else {
+                for (int i = ligne1; i < ligne2; i++) {
+                    if (echiquier[i][colonne1] != null) {
+                        return false;
+                    }
+
+                }
             }
-
-
-            if (pos1.estSurLaMemeColonneQue(pos2)) {
-                if (ligne1 > ligne2) {
-                    for (int i = ligne1; i < ligne2; i--) {
-                        if (echiquier[i][colonne1] != null) {
-                            return false;
-                        }
-
+        } else if (pos1.estSurLaMemeLigneQue(pos2)) {
+            if (colonne1 > colonne2) {
+                for (int i = colonne1; i > colonne2; i--) {
+                    if (echiquier[ligne1][i] != null) {
+                        return false;
                     }
-                } else {
-                    for (int i = ligne1; i > ligne2; i++) {
-                        if (echiquier[i][colonne1] != null) {
-                            return false;
-                        }
 
-                    }
                 }
-            } else if (pos1.estSurLaMemeLigneQue(pos2)) {
-                if (colonne1 > colonne2) {
-                    for (int i = colonne1; i < colonne2; i--) {
-                        if (echiquier[ligne1][i] != null) {
-                            return false;
-                        }
-
-                    }
-                } else {
-                    for (int i = colonne1; i > colonne2; i++) {
-                        if (echiquier[ligne1][i] != null) {
-                            return false;
-                        }
-
+            } else {
+                for (int i = colonne1; i < colonne2; i++) {
+                    if (echiquier[ligne1][i] != null) {
+                        return false;
                     }
 
                 }
 
-
+            }
             return true;
         }
         return false;
